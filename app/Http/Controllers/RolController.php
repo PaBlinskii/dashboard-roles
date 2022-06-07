76413165c747c2026916a4e86c80568f3e8132f3
Pablo Antonifram <ipxb10@gmail.com>
2022-06-06 21:36:13 -0600

    Terminando proyecto de Roles y Dashboard

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// agregamos Spatie
+//agregamos
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usuario = User::create([
+            'name' => 'Pantoni Franki',
+            'email' => 'pantonix@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        // $rol = Role::create(['name' => 'Administrador']);
+
+        // $permisos= Permission::pluck('id', 'id')->all();
+
+        // $rol->syncPermissions($permisos);
+
+        // $usuario->assignRole([$rol->id]);
+        
+        // Si ya existe rol entonces se usa lo siguiente para asignarlo al Admin
+        $usuario->assignRole('Administrador');
+    }
+}
